a3e20344ed82e3171f29e10bf691eba60a6fb5bd
Samuel Miller Soares <samuel.m.soares10@aluno.senai.br>
2025-05-20 16:05:21 -0300

    Delete Lista de Exercicios - PBE - Operadores Aritimeticos e Relacionais directory

--- Lista de Exercicios - PBE - Operadores Aritimeticos e Relacionais/exercicio05.py
+++
@@ -1,4 +0,0 @@
-#EXERCICIO 05 - CONVERSOR DE VARIÁVEL
-numero_txt = "150"
-numero = int(numero_txt)
-print(f"O resultado de {numero_txt} * 2 é {numero*2}.")
--- Lista de Exercicios - PBE - Operadores Aritimeticos e Relacionais/exercicio06.py
+++
@@ -1,6 +0,0 @@
-#EXERCICIO 06 - LISTA DE NÚMEROS
-numeros = [1, 2, 3, 4, 5]
-print(numeros)
-numeros[1] = 20
-print(numeros)
-
